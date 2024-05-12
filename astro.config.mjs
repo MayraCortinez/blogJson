1078072e10b51c425d2e912ad9a0b4ac5c9db8c8
MayC <mayrautopia@gmail.com>
2024-05-12 16:44:21 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static', /* defino el modo de renderizado, la opción static es por defecto, si necesito que se renderice por servidor debo colocar en la página que la necesita export const prerender = false */
+  integrations: [tailwind()],
+  jsx: 'react'
+});
