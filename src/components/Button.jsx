959db2bdda08468bc9dfef01b91aac875ea87f05
MayC <mayrautopia@gmail.com>
2024-05-12 18:53:08 -0300

    add viewTransitions end first part

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+export function Button() {
+    return (
+        <button onClick={() => {
+            alert('Gracias por hacer click')
+        }}>
+            Hola! Hazme click
+        </button>
+    )
+}
